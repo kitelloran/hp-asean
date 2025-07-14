e58b2527976e93c5d4ba4d5252ed669a4a22ea25
Kit Elloran <cbelloran@aseanbiodiversity.org>
2025-07-14 17:15:02 +0800

    Update config.js

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -8,7 +8,6 @@ var siteConfig = {
   version: 2,
   routes: {
     alwaysUseHrefs: true, // Update - there now is translations. since the site isn't translated we can use push for now. if true, then we will always use hrefs, if false we will use onClick events and push state to the history. I've added this because I just realize that the language picker doesn't work with pushState as the url of the translated site is not updated with the new url
-    highlightedFilters: ['q', 'datasetKey', 'taxonKey', 'occurrenceStatus', 'basisOfRecord', 'year', 'samplingProtocol', 'depth', 'occurrenceIssue', 'networkKey'],
     enabledRoutes: ['occurrenceSearch', 'collectionSearch', 'collectionKey', 'institutionSearch', 'institutionKey'],
   },
   occurrence: {
@@ -20,6 +19,8 @@ var siteConfig = {
           "key": "country",
           "values": ["BN", "KH", "ID", "LA", "MY", "MM", "PH", "SG", "TH", "VN"]
         },
+        occurrenceSearchTabs: ['TABLE', 'GALLERY', 'MAP', 'DATASETS', 'DASHBOARD', 'CLUSTERS'],
+        highlightedFilters: ['q', 'datasetKey', 'taxonKey', 'occurrenceStatus', 'basisOfRecord', 'year', 'samplingProtocol', 'depth', 'occurrenceIssue', 'networkKey'],
         {
           "type": "equals",
           "key": "notIssues",
