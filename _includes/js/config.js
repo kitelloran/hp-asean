e5bb0f1e96b6d47ac03eab0f1415bcb0353352c1
Kit Elloran <cbelloran@aseanbiodiversity.org>
2025-07-14 18:02:28 +0800

    Update config.js

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -13,7 +13,7 @@ var siteConfig = {
   },
   occurrence: {
     occurrenceSearchTabs: ['TABLE', 'GALLERY', 'MAP', 'DATASETS', 'DASHBOARD', 'CLUSTERS'],
-    highlightedFilters: ['q', 'country', 'datasetKey', 'taxonKey', 'occurrenceStatus', 'basisOfRecord', 'year', 'samplingProtocol', 'depth', 'occurrenceIssue', 'networkKey'],
+    highlightedFilters: ['q', 'country', 'datasetKey', 'taxonKey', 'occurrenceStatus', 'basisOfRecord', 'year'],
     rootPredicate: {
       "type": "and",
       "predicates": [
