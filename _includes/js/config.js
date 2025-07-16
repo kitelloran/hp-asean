75448cd8aa4c4fa24fad9bb00037be4a71be7bad
Kit Elloran <cbelloran@aseanbiodiversity.org>
2025-07-16 10:48:48 +0800

    Update config.js

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -33,12 +33,13 @@ var siteConfig = {
       lat: 10.537453, // Latitude of Map
       lng: 114.242062, // Longitude of Map
       zoom: 4 // Level of zoom adjusted to 4
-    },
+    }, // ACB : a30d7f59-d3d4-4e89-97dc-de9cf837f591
     // You probably need help to configure the scope - so just ask
     // for his demo site we only show Fungi (taxonKey=5). It use the predicate structure known from GBIF download API. 
     // See https://www.gbif.org/developer/occurrence (long page without enough anchors - search for "Occurrence Download Predicates")
     // The format is however slightly different, in that is use camelCase for keys instead of CONSTANT_CASE. 
-    rootPredicate: { type: 'equals', key: 'networkKey', value: '2b7c7b4f-4d4f-40d3-94de-c28b6fa054a6' }, 
+    // rootPredicate: { type: 'equals', key: 'networkKey', value: '2b7c7b4f-4d4f-40d3-94de-c28b6fa054a6' }, 
+    rootPredicate: { type: 'equals', key: 'publisher', value: 'a30d7f59-d3d4-4e89-97dc-de9cf837f591' }, 
     // occurrenceSearchTabs: ['MAP', 'TABLE', 'GALLERY', 'DATASETS'] // what tabs should be shown
     // see https://hp-theme.gbif-staging.org/data-exploration-config for more options
   }
