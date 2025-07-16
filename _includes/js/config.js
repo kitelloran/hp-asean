1a379c5da5e414c9054ee43a922d5b15b2d63057
Kit Elloran <cbelloran@aseanbiodiversity.org>
2025-07-16 12:21:57 +0800

    Update config.js

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -26,11 +26,6 @@ var siteConfig = {
           "type": "equals",
           "key": "notIssues",
           "value": "COUNTRY_COORDINATE_MISMATCH"
-        },
-        { 
-          "type": "equals", 
-          "key": "country", 
-          "values": ["BN", "KH", "ID", "LA", "MY", "MM", "PH", "SG", "TH", "VN", "TL"]
         }
       ]
     }, // End Occurrence Function
@@ -38,17 +33,17 @@ var siteConfig = {
       lat: 10.537453, // Latitude of Map
       lng: 114.242062, // Longitude of Map
       zoom: 4 // Level of zoom adjusted to 4
-    }//, // ACB : a30d7f59-d3d4-4e89-97dc-de9cf837f591
+    },//, // ACB : a30d7f59-d3d4-4e89-97dc-de9cf837f591
     // You probably need help to configure the scope - so just ask
     // for his demo site we only show Fungi (taxonKey=5). It use the predicate structure known from GBIF download API. 
     // See https://www.gbif.org/developer/occurrence (long page without enough anchors - search for "Occurrence Download Predicates")
     // The format is however slightly different, in that is use camelCase for keys instead of CONSTANT_CASE. 
-    //rootPredicate: 
-     // { 
-       // type: 'equals', 
-       // key: 'country', 
-       // value: ["BN", "KH", "ID", "LA", "MY", "MM", "PH", "SG", "TH", "VN", "TL"]
-      //}, 
+    rootPredicate: 
+     { 
+      "type": "equals", 
+      "key": "country", 
+      "values": ["BN", "KH", "ID", "LA", "MY", "MM", "PH", "SG", "TH", "VN", "TL"]
+    }
     //rootPredicate: { type: 'equals', key: 'publisher', value: 'a30d7f59-d3d4-4e89-97dc-de9cf837f591' }, 
     // occurrenceSearchTabs: ['MAP', 'TABLE', 'GALLERY', 'DATASETS'] // what tabs should be shown
     // see https://hp-theme.gbif-staging.org/data-exploration-config for more options
